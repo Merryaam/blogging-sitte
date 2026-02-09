063157115cf53bfe7288e15a324d6c1f3f2abe50
Merryaam <merryaam@gmail.com>
2026-02-09 15:07:51 +0200

    Add README with project details

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ console.log("userRoutes is:", typeof userRoutes);
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }
+)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
